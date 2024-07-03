258f4bf2f87ae430d7506578843030ca7929663d
sophie ranaivoharisoa <sophie.rhf@gmail.com>
2024-07-03 16:48:55 +0200

    yelp mvp

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,15 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+hashimoto = {name: "Hashimoto", address: "Fröschchengasse 14, 66111 Saarbrücken", phone_number: "+49 185 9735 2938 28", category: "japanese"}
+pizza_per_tutti = {name: "Pizza per Tutti", address: "Saargemünder Str. 145, 66119 Saarbrücken", phone_number: "+49 681 6453 288", category: "italian"}
+kalinski = {name: "Kalinski", address: "Kingsroad 56a, 72 London", phone_number: "+4474395959832", category: "belgian"}
+brasserie = {name: "Brasserie", address: "Rue de Belfort, 5837 Paris", phone_number: "+33243995827282", category: "french"}
+dim_sum = {name: "Dim Sum", address: "Tree Avenue 57, New York", phone_number: "+66 43783249385", category: "chinese"}
+
+[hashimoto, pizza_per_tutti, kalinski, brasserie, dim_sum].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
